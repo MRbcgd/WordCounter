a35c6042054c637e07ef08bcf1621758f163b4db
MRbcgd <quittypig@gmail.com>
2017-01-04 11:13:04 +0900

    1. Split the words in all text on the web page. 2. Calculate the word frequency.

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
+//select word. exclude source code
 var entireText=document.getElementsByTagName('body')[0].innerText;
+//split word
 var splitedText=entireText.split(' ');
+
+var countedWord={};//js object
+//insert splited word into object
+for(var i=0; i<splitedText.length; i++){
+	var word=splitedText[i];
+  //counting word
+	if(countedWord[word]==undefined){
+	    countedWord[word]=1;
+    }
+	else{
+		countedWord[word]++;
+    }
+}
+console.log(countedword);
