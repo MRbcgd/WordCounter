f7a828035fccf589630a7dc33d36d0ab00b47fd3
MRbcgd <quittypig@gmail.com>
2017-01-04 12:04:18 +0900

    3. Output the result after sorting.

--- main.js
+++ main.js
@@ -10,9 +10,18 @@ for(var i=0; i<splitedText.length; i++){
   //counting word
 	if(countedWord[word]==undefined){
 	    countedWord[word]=1;
-    }
+  }
 	else{
 		countedWord[word]++;
-    }
+  }
 }
-console.log(countedword);
+
+//Make it an array for sorting.
+var countedWordArr=new Array();
+for(var name in countedWord){
+	countedWordArr.push([name,countedWord[name]])
+}
+//ASC sorting
+countedWordArr.sort(function(a,b){
+	return a[1]-b[1];
+})
